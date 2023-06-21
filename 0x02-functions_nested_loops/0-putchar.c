b28eecc9697c93f397bad5cd84aca5752feec8b4
Cheri2-5 <c.achieng125@gmail.com>
2023-06-21 05:35:43 -0700

    Writing a program that prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include main.h
+/**
+ * description 'prints _putchar to stdout
+ * return 0
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return(0);
+}	
